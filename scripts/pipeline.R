d26d04a5805589ba8f7d2c31a5062d59bb0d684c
aereinh <aereinh@unc.edu>
2024-12-01 18:56:48 -0500

    updated prediction pipeline

--- scripts/pipeline.R
+++ scripts/pipeline.R
@@ -6,6 +6,7 @@ library(logisticPCA)
 library(sf)
 library(rnaturalearth)
 library(rnaturalearthdata)
+library(rlist)
 
 # Data Cleaning ------------------------------------------------------------
 
@@ -43,6 +44,7 @@ hist(df_model$Fws)
 hist((df_model$Fws_logit))
 
 # impute missing resistances
+set.seed(123)
 outcome_vars <- grep("resistant$", names(df_model), value = T)
 df_toImpute <- df_model %>%
   select(all_of(outcome_vars), Monoclonal, country)
@@ -67,7 +69,7 @@ df_pca %>%
   theme_minimal()
 
 set.seed(23)
-nclusters <- 5
+nclusters <- 6
 #x_cluster <- df_pca[c("Monoclonal","PC1","PC2","long","lat")]
 x_cluster <- df_pca[c(outcome_vars,"Monoclonal","long","lat")]
 kmeans_res <- kmeans(x = x_cluster, centers = nclusters)
@@ -104,23 +106,234 @@ ggplot(data = map_data) +
 
 write.csv(df_clusters, '../data/pf7_clustered.csv')
 
-# add number of resistances
-df_final <- df_clusters %>%
-  mutate(nresistances = CQresistant + MQresistant + ARTresistant + PPQresistant + PYRresistant + SDXresistant) %>%
-  mutate(resistant1 = ifelse(nresistances >= 1, 1, 0)) %>%
-  mutate(resistant2 = ifelse(nresistances >= 2, 1, 0)) %>%
-  mutate(resistant3 = ifelse(nresistances >= 3, 1, 0)) %>%
-  mutate(resistant4 = ifelse(nresistances >= 4, 1, 0)) %>%
-  mutate(resistant5 = ifelse(nresistances >= 5, 1, 0)) %>%
-  mutate(resistant6 = ifelse(nresistances >= 6, 1, 0))
-
-write.csv(df_final, '../data/pf7_prediction.csv')
+df_final <- df_clusters
+# df_final <- df_clusters %>%
+#   mutate(nresistances = CQresistant + MQresistant + ARTresistant + PPQresistant + PYRresistant + SDXresistant) %>%
+#   mutate(resistant1 = ifelse(nresistances >= 1, 1, 0)) %>%
+#   mutate(resistant2 = ifelse(nresistances >= 2, 1, 0)) %>%
+#   mutate(resistant3 = ifelse(nresistances >= 3, 1, 0)) %>%
+#   mutate(resistant4 = ifelse(nresistances >= 4, 1, 0)) %>%
+#   mutate(resistant5 = ifelse(nresistances >= 5, 1, 0)) %>%
+#   mutate(resistant6 = ifelse(nresistances >= 6, 1, 0))
+# 
+# write.csv(df_final, '../data/pf7_prediction.csv')
   
+df_long = df_final %>%
+  pivot_longer(cols = ends_with("resistant"), names_to = "drug", values_to = "resistance") %>%
+  mutate(resistance_f = factor(ifelse(resistance == 1, "Yes", "No"), levels = c("Yes","No"))) %>%
+  #mutate(resistance_f = factor(resistance, levels = c(1,0))) %>%
+  mutate(drug = factor(sub("resistant.*$", "", drug))) %>%
+  mutate(region_grp = factor(region_grp, levels = 1:6))
+
+# resistance frequencies
+df_long %>%
+  ggplot(aes(x = drug, fill = resistance_f))+
+  geom_bar(position = "fill")+
+  facet_wrap(vars(region_grp),
+             labeller = as_labeller(function(label) paste("Region", label)))+
+  ylab("Proportion")+
+  xlab("Drug")+
+  labs(fill = "Resistant")+
+  ggtitle('Relative frequency of drug resistance')
+
+# resistance on Monoclonal trends
+df_long %>%
+  ggplot(aes(x = Monoclonal, y = resistance, col = factor(region_grp)))+
+  stat_smooth(method = "glm", se = F, method.args = "binomial")+
+  facet_wrap(vars(drug))+
+  labs(col = "Region")+
+  theme_bw()+
+  ylab('Reistance')+
+  ggtitle('Fitted resistance probabilities')
+
+df_long %>%
+  group_by(region_grp, drug) %>%
+  summarise(sd_x = sd(Monoclonal), sd_y = sd(resistance))
+
+# analyze variation in outcome by group
+df_grp = df_long %>%
+  group_by(drug, region_grp, Monoclonal) %>%
+  summarise(n = n(), sd_y = sd(resistance), .groups = "keep")
+df_grp_novar = df_grp %>% filter(sd_y == 0)
+
+df_long_rm = df_long %>%
+  group_by(drug, region_grp, Monoclonal) %>%
+  mutate(sd_y = sd(resistance)) %>%
+  filter(sd_y != 0)
+
+write.csv(df_long_rm, '../data/pf7_prediction.csv')
+  
+
+# Fit Full Models ---------------------------------------------------------
+df_long_rm = read.csv('../data/pf7_prediction.csv')
+
+fit_models = function(data, outcome=NULL) {
+  if (!is.null(outcome)) {
+    data_o = data %>% filter(drug == outcome)
+  } else {
+    data_o = data
+  }
+  mod_main_o = glm(resistance ~ Monoclonal+region_grp, data = data_o, family = "binomial")
+  mod_int_o = glm(resistance ~ Monoclonal*region_grp, data = data_o, family = "binomial")
+  mod_ri_o = glmer(resistance ~ Monoclonal + (1|region_grp), data = data_o, family = "binomial")
+  mod_rs_o = glmer(resistance ~ Monoclonal + (Monoclonal|region_grp), data = data_o, family = "binomial")
+  Model_list = list("Fixed_Main"=mod_main_o,
+                    "Fixed_Int"=mod_int_o,
+                    "Mixed_RI"=mod_ri_o,
+                    "Mixed_RS"=mod_rs_o)
+  return(Model_list)
+}
+
+res_art = fit_models(df_long_rm, outcome = "ART")
+res_cq = fit_models(df_long_rm, outcome = "CQ")
+res_mq = fit_models(df_long_rm, outcome = "MQ")
+res_ppq = fit_models(df_long_rm, outcome = "PPQ")
+res_pyr = fit_models(df_long_rm, outcome = "PYR")
+res_sdx = fit_models(df_long_rm, outcome = "SDX")
+
+# get AICs
+unlist(lapply(res_art, AIC))
+unlist(lapply(res_cq, AIC))
+unlist(lapply(res_mq, AIC))
+unlist(lapply(res_ppq, AIC))
+unlist(lapply(res_pyr, AIC))
+unlist(lapply(res_sdx, AIC))
+
+
+
+# Prediction --------------------------------------------------------------
+fit_models_cv = function(data, outcome=NULL) {
+  if (!is.null(outcome)) {
+    data_o = data %>% filter(drug == outcome)
+  } else {
+    data_o = data
+  }
+  mod_main_o = glm(resistance ~ Monoclonal+region_grp, data = data_o, family = "binomial")
+  mod_int_o = glm(resistance ~ Monoclonal*region_grp, data = data_o, family = "binomial")
+  mod_ri_o = glmer(resistance ~ Monoclonal + (1|region_grp), data = data_o, family = "binomial")
+  mod_rs_o = glmer(resistance ~ Monoclonal + (Monoclonal|region_grp), data = data_o, family = "binomial")
+  Model_list = list("Fixed_Main"=mod_main_o,
+                    "Fixed_Int"=mod_int_o,
+                    "Mixed_RI"=mod_ri_o,
+                    "Mixed_RS"=mod_rs_o)
+  return(Model_list)
+}
+get_folds = function(data, groups, nfolds = 10) {
+  data$group = do.call(paste, c(data[groups], sep="_"))
+  data$fold = NA
+  unique_groups = unique(data$group)
+  for (g in unique_groups) {
+    group_indices = which(data$group == g)
+    folds = createFolds(group_indices, k=nfolds, list=T)
+    for (fold_idx in seq_along(folds)) {
+      data$fold[group_indices[folds[[fold_idx]]]] <- fold_idx
+    }
+  }
+  return(data)
+}
+get_summ_metrics = function(y, probs) {
+  yhat <- ifelse(probs >= .5, 1, 0)
+  TP <- sum(yhat[y==1])
+  TN <- sum(yhat[y==0]==0)
+  FP <- sum(yhat[y==0]==1)
+  FN <- sum(yhat[y==1]==0)
+  n <- length(y)
+  accuracy <- (TP + TN) / n
+  sensitivity <- TP / (TP + FN)
+  specificity <- TN / (TN + FP)
+  precision <- TP / (TP + FP)
+  if (TP + FP + FN > 0) {
+    f1_score <- 2 * (precision * sensitivity) / (precision + sensitivity)
+  } else {
+    f1_score <- NA
+  }
+  RMSE <- sqrt(mean((y-probs)^2))
+  res <- data.frame(accuracy = accuracy,
+                    F1_score = f1_score,
+                    sensitivity = sensitivity,
+                    specificity = specificity,
+                    RMSE = RMSE)
+  return(res)
+}
+model_predict = function(mod, newdata) {
+  probs = predict(mod, newdata, type = "response")
+  y = newdata$resistance
+  return(get_summ_metrics(y, probs))
+}
+cross_validation = function(data, outcome, groups, nfolds = 10, nreps=1, seed = 123) {
+  pred_res = vector(mode = "list", 4)
+  for (r in 1:nreps) {
+    print(paste0('Replicate = ', r))
+    set.seed(seed + r)
+    df_cv = data %>% filter(drug == outcome) %>% get_folds(groups, nfolds)
+    for (k in 1:nfolds) {
+      print(paste0('Fold = ',k))
+      df_train_k = df_cv %>% filter(fold != k)
+      df_test_k = df_cv %>% filter(fold == k)
+      mods_k = fit_models_cv(df_train_k)
+      preds_k = lapply(mods_k, function(x) cbind(model_predict(x, df_test_k), data.frame("fold"=k, "rep"=r)))
+      pred_res = lapply(1:4, function(x) {
+        rbind(pred_res[[x]], preds_k[[x]])
+      })
+    }
+  }
+  names(pred_res) = names(preds_k)
+  return(pred_res)
+}
+
+df_long_rm = read.csv('../data/pf7_prediction.csv')
+groups = c("resistance", "Monoclonal", "region_grp")
+unique(df_long_rm$drug)
+
+# Run 10-fold cross-validation
+#cv_res_art = cross_validation(df_long_rm, "ART", groups, nfolds = 10)
+cv_res_cq = cross_validation(df_long_rm, "CQ", groups, nfolds = 10, nreps = 3, seed = 123)
+#cv_res_ppq = cross_validation(df_long_rm, "PPQ", groups, nfolds = 10)
+#cv_res_pyr = cross_validation(df_long_rm, "PYR", groups, nfolds = 10)
+#cv_res_sdx = cross_validation(df_long_rm, "SDX", groups, nfolds = 10)
+#cv_res_mq = cross_validation(df_long_rm, "MQ", groups, nfolds = 10)
+
+#list.save(cv_res_cq, '../cv_results/CQPrediction.rdata')
+
+
+# Plot Results ------------------------------------------------------------
+library(ggpubr)
+
+#cv_res_cq = list.load('../cv_results/CQPrediction.rdata')
+df_cv_res_cq = do.call(rbind, lapply(names(cv_res_cq), function(model_name) {
+  df = cv_res_cq[[model_name]]
+  df$Model = model_name
+  return(df)
+}))
+df_cv_plot_cq = df_cv_res_cq %>%
+  mutate(Model = factor(case_when(Model == "Fixed_Int" ~ "Fixed Effects w/ interaction",
+                                  Model == "Fixed_Main" ~ "Fixed Effects",
+                                  Model == "Mixed_RI" ~ "Random Intercept",
+                                  Model == "Mixed_RS" ~ "Random Slope")))
+
+p = df_cv_plot_cq %>%
+  ggplot(aes(x = Model, y = F1_score, fill = Model))+
+  geom_boxplot()+
+  labs(fill = "Model")+
+  theme_bw(base_size=18)+
+  ylab('F1 Score')+
+  theme(axis.text.x = element_blank(), axis.ticks.x = element_blank())+
+  xlab('')
+stat_test = compare_means(F1_score ~ Model, data = df_cv_plot_cq, method = "t.test")
+p = p+stat_compare_means(
+  comparisons = list(c("Fixed Effects w/ interaction", "Random Intercept")),
+  method = "t.test",
+  label = "p.signif"
+)
+p
+
 
 
 # Prediction (10-fold Cross-Validation) --------------------------------------------------------------
-df_final = read.csv('data/pf7_prediction.csv')
-data = df_final %>% na.omit()
+df_final = read.csv('../data/pf7_prediction.csv')
+data = df_final %>% na.omit() %>%
+  mutate(site_id = factor(site_id)) %>%
+  mutate(country = factor(country))
 
 remove_zerovar <- function(data, groups) {
   y_var <- groups[1]
@@ -219,6 +432,46 @@ cross_validate <- function(form, data, family="binomial",
   }
   return(res_kfold)
 }
+predict_outcome <- function(data, nfolds = 10, family = "binomial", outcome="CQresistant", cov="Monoclonal", grp="region_grp") {
+  form_fixed <- as.formula(paste0(outcome, " ~ ", cov, " + ", grp))
+  form_randint <- as.formula(paste0(outcome, " ~ ", cov, " + (1|", grp,")"))
+  form_randslope <- as.formula(paste0(outcome, " ~ ", cov, " + (1 + ",cov,"|", grp,")"))
+  cv_res_fixed <- cross_validate(form_fixed, data, family = family, nfolds=nfolds)
+  cv_res_randint <- cross_validate(form_randint, data, family = family, nfolds=nfolds)
+  cv_res_randslope <- cross_validate(form_randslope, data, family = family, nfolds=nfolds)
+  cv_res_fixed$Model <- "Fixed Effects"
+  cv_res_randint$Model <- "Random Intercept"
+  cv_res_randslope$Model <- "Random Slope"
+  cv_res <- rbind(cv_res_fixed, cv_res_randint, cv_res_randslope)
+  cv_res$Outcome <- outcome
+  return(cv_res)
+}
+
+# Predict binary resistance outcomes with region-level
+set.seed(123)
+outcomes <- names(data)[endsWith(names(data), "resistant")]
+cov = "Monoclonal"
+grp <- "region_grp"
+cv1 <- predict_outcome(data, nfolds = 25, family = "binomial", outcome = outcomes[1], cov = cov, grp = grp)
+cv2 <- predict_outcome(data, nfolds = 25, family = "binomial", outcome = outcomes[2], cov = cov, grp = grp)
+cv3 <- predict_outcome(data, nfolds = 25, family = "binomial", outcome = outcomes[3], cov = cov, grp = grp)
+cv4 <- predict_outcome(data, nfolds = 25, family = "binomial", outcome = outcomes[4], cov = cov, grp = grp)
+cv5 <- predict_outcome(data, nfolds = 25, family = "binomial", outcome = outcomes[5], cov = cov, grp = grp)
+cv6 <- predict_outcome(data, nfolds = 25, family = "binomial", outcome = outcomes[6], cov = cov, grp = grp)
+cv_res <- rbind(cv1, cv2, cv3, cv4, cv5, cv6)
+
+# Plot results
+#cv_res <- cv1
+cv_res %>%
+  ggplot(aes(y = accuracy, fill = factor(Model)))+
+  facet_wrap(vars(Outcome))+
+  geom_boxplot()+
+  theme_bw()+
+  labs(fill = "Model")+
+  ylab("Accuracy")
+
+
+# Misc --------------------------------------------------------------------
 
 
 # Model if 3 or more resistances are observed by transformed Fws (COI) and country
